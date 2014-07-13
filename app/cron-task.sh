6bfbc5426897013edb7eb7a19237148ada519231
Vlad GURDIGA <gurdiga@gmail.com>
2014-07-13 18:41:55 +0300

    cron task: align with the file organization changes

--- app/cron-task.sh
+++ app/cron-task.sh
@@ -3,4 +3,4 @@
 set -e
 
 cd `dirname $0`
-/usr/local/bin/node `pwd`/index.js "cebanu valentina:CA Chisinau" | mail -s 'PINJ updates' gurdiga@gmail.com
+/usr/local/bin/node `pwd`/app/index.js "cebanu valentina:CA Chisinau" | mail -s 'PINJ updates' gurdiga@gmail.com

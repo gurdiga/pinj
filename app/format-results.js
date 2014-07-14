f5d327130aca3041ed6ee2e25c283b7484e1d86e
Vlad GURDIGA <gurdiga@gmail.com>
2014-07-14 12:08:57 +0300

    global search for agenda şedinţelor

--- app/format-results.js
+++ app/format-results.js
@@ -5,14 +5,6 @@ var strings = require('./strings.json');
 var _ = require('underscore');
 
 module.exports = function(data) {
-  _(data.results).each(function(result, id) {
-    try {
-      data.results[id] = JSON.parse(result);
-    } catch(e) {
-      data.results[id] = { error: result };
-    }
-  });
-
   var jadeOptions = { pretty: true};
 
   _(data).extend(jadeOptions);

35fdf4beef77db097f26612af7e367f54ce19477
Vlad GURDIGA <gurdiga@gmail.com>
2014-07-15 11:19:56 +0300

    clone.js

--- app/sections/agendaŞedinţelor.js
+++ app/sections/agendaŞedinţelor.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var clone = require('../clone');
 var instanţe = require('../meta').instanţe;
 var querySection = require('../query-section');
 
 module.exports = function agendaŞedinţelor(query) {
+  instanţe = clone(instanţe);
+
   // pare să nu fie implementată pentru Slobozia
   // http://instante.justice.md/cms/curtea-de-apel-bender/jslb-menu
   delete instanţe.jslb;

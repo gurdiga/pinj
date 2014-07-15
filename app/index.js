c6e893bc03cc54948db4d4f104bceee0feb0b8a2
Vlad GURDIGA <gurdiga@gmail.com>
2014-07-15 11:20:57 +0300

    refactoring for clarity

--- app/index.js
+++ app/index.js
@@ -1,32 +1,44 @@
 'use strict';
 
 var _ = require('underscore');
-var forEach = require('async').eachSeries;
-var parallelize = require('async').parallel;
-var sections = require('./meta').sections;
-var queries = getQueries();
+var async = require('async');
 
-forEach(queries, function(query, callback) {
-  parallelize(sectionQueries(query), printResults(query, callback));
-});
+var globalQueries = prepareGlobalQueries();
+async.series(globalQueries, printResults);
 
 
-function getQueries() {
-  var args = process.argv.slice(2);
+function prepareGlobalQueries() {
+  var queries = process.argv.slice(2);
 
-  if (args.length === 0) {
-    showUsageAndExit();
-    process.exit(1);
-    return;
-  }
+  if (queries.length === 0) showUsageAndExit();
 
-  return args;
+  return taskList(queries, doSectionQueries);
 }
 
-function sectionQueries(query) {
-  return _.chain(sections)
-    .map(function(data, sectionId) {
-      return [sectionId, require('./sections/' + sectionId)(query)];
+function doSectionQueries(query) {
+  return function(callback) {
+    var sectionQueries = prepareSectionQueries(query);
+    async.parallel(sectionQueries, callback);
+  };
+}
+
+function prepareSectionQueries(query) {
+  var sections = require('./meta').sections;
+  var sectionIds = _(sections).keys();
+
+  return taskList(sectionIds, doSectionQuery(query));
+}
+
+function doSectionQuery(query) {
+  return function(sectionId) {
+    return require('./sections/' + sectionId)(query);
+  };
+}
+
+function taskList(labels, iterator) {
+  return _.chain(labels)
+    .map(function(label) {
+      return [label, iterator(label)];
     })
     .object()
     .value();
@@ -39,21 +51,15 @@ function showUsageAndExit() {
     '  ' + process.argv[1] + ' "text de căutat:curtea de apel" ...' +
     '\n'
   );
+  process.exit(1);
 }
 
-function printResults(query, callback) {
+function printResults(err, results) {
   var formatResults = require('./format-results');
 
-  return function(err, results) {
-    if (err) {
-      console.error(query, err);
-    } else {
-      console.log(formatResults({
-        'query': query,
-        'results': results
-      }));
-    }
-
-    callback(null);
-  };
+  if (err) {
+    console.error('printResults error:', err);
+  } else {
+    console.log(formatResults({ 'results': results }));
+  }
 }

3de55668b868f3cc314eb92889907e9b954e2ade
Vlad GURDIGA <gurdiga@gmail.com>
2014-07-21 10:21:53 +0300

    moved some more code to sections

--- app/sections/cereriÎnInstanţă.js
+++ app/sections/cereriÎnInstanţă.js
@@ -1,9 +1,33 @@
 'use strict';
 
+var SECTION_ID = 'cereriÎnInstanţă';
+
+var format = require('util').format;
+
+var clone = require('../clone');
+var sections = require('../meta').sections;
 var querySection = require('../query-section');
 
-module.exports = function cereriÎnInstanţă(query) {
+module.exports.query = function(query) {
   var courts = {'url-unic': 'Cereri în instanţă'};
 
-  return querySection('cereriÎnInstanţă', query, courts);
+  return querySection(SECTION_ID, query, courts);
+};
+
+module.exports.preprocess = function() {
+};
+
+module.exports.getSearchOptions = function(query) {
+  var searchOptions = clone(sections[SECTION_ID].searchOptions);
+  var currentYear = (new Date()).getFullYear();
+
+  searchOptions.filters = JSON.stringify(searchOptions.filters)
+    .replace(/%QUERY%/g, query)
+    .replace(/%CURRENT_YEAR%/g, currentYear);
+
+  return searchOptions;
+};
+
+module.exports.getUrl = function(courtId) {
+  return format(sections[SECTION_ID].urlFormat, courtId);
 };
--- app/sections/citaţiiÎnInstanţă.js
+++ app/sections/citaţiiÎnInstanţă.js
@@ -1,9 +1,49 @@
 'use strict';
 
+var SECTION_ID = 'citaţiiÎnInstanţă';
+
+var format = require('util').format;
+
+var clone = require('../clone');
+var sections = require('../meta').sections;
 var querySection = require('../query-section');
 
-module.exports = function citaţiiÎnInstanţă(query) {
+module.exports.query = function(query) {
   var courts = {'url-unic': 'Cereri în instanţă'};
 
-  return querySection('citaţiiÎnInstanţă', query, courts);
+  return querySection(SECTION_ID, query, courts);
+};
+
+module.exports.preprocess = function(row, query) {
+  var name, role;
+
+  var accuser = row.cell[6];
+  var culprit = row.cell[4];
+  var foundInCulprit = culprit.indexOf(query) > -1;
+
+  if (foundInCulprit) {
+    name = culprit;
+    role = 'pîrît';
+  } else {
+    name = accuser;
+    role = 'reclamant';
+  }
+
+  row.cell[102] = name;
+  row.cell[103] = role;
+};
+
+module.exports.getSearchOptions = function(query) {
+  var searchOptions = clone(sections[SECTION_ID].searchOptions);
+  var currentYear = (new Date()).getFullYear();
+
+  searchOptions.filters = JSON.stringify(searchOptions.filters)
+    .replace(/%QUERY%/g, query)
+    .replace(/%CURRENT_YEAR%/g, currentYear);
+
+  return searchOptions;
+};
+
+module.exports.getUrl = function(courtId) {
+  return format(sections[SECTION_ID].urlFormat, courtId);
 };

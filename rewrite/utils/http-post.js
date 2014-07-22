8e60680d201b60464f1ed256e4799fb717adcfd1
Vlad GURDIGA <gurdiga@gmail.com>
2014-07-22 15:11:02 +0300

    refactoring for null-object-like body

--- rewrite/utils/http-post.js
+++ rewrite/utils/http-post.js
@@ -4,21 +4,35 @@ var Q = require('q');
 var request = require('request');
 
 function httpPost(url, formData) {
-  var deferred = Q.defer();
-
-  request({
+  var requestOptions = {
     uri: url,
     method: 'POST',
     json: true,
     form: formData
-  }, function(err, res, body) {
-    if (err) console.error(err, url, formData);
-    if (res.statusCode !== 200) console.error('HTTP', res.statusCode, url, formData);
+  };
+  var deferred = Q.defer();
 
-    deferred.resolve(body);
-  });
+  request(requestOptions, passResponseDataTo(deferred));
 
   return deferred.promise;
+
+  function passResponseDataTo(deferred) {
+    return function(err, res, body) {
+      if (err) console.error(err, url, formData);
+      if (res.statusCode !== 200) console.error('HTTP', res.statusCode, url, formData);
+
+      deferred.resolve(prepareBody(body));
+    };
+  }
+
+  function prepareBody(body) {
+    var emptyBody = { rows: [] };
+
+    body = body || emptyBody;
+    body.rows = body.rows || emptyBody.rows;
+
+    return body;
+  }
 }
 
 module.exports = httpPost;

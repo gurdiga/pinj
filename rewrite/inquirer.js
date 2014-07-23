8897b6b230fd4ac9a7d12a2b58b8eea5058df875
Vlad GURDIGA <gurdiga@gmail.com>
2014-07-23 18:06:34 +0300

    rewrite: make Inquirer a module

--- rewrite/inquirer.js
+++ rewrite/inquirer.js
@@ -5,25 +5,20 @@ var CaseInquirySection = require('./sections/case-inquiry-section');
 var SummonsSection = require('./sections/summons-section');
 var SentenceSection = require('./sections/sentence-section');
 
-function Inquirer() {
-}
+var Inquirer = {};
 
-Inquirer.sections = [
+var sections = [
   new AgendaSection(),
   new CaseInquirySection(),
   new SummonsSection(),
   new SentenceSection()
 ];
 
-Inquirer.prototype.getSections = function() {
-  return Inquirer.sections;
-};
-
-Inquirer.prototype.inquireAbout = function(clientNames) {
+Inquirer.inquireAbout = function(clientNames) {
   var forEach = require('./utils/for-each');
 
   return forEach(clientNames).inSeries(function(clientName) {
-    return forEach(this.getSections()).inParallel(function(section) {
+    return forEach(sections).inParallel(function(section) {
       return section.inquireAbout(clientName);
     });
   }, this);

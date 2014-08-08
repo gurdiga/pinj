6b0edcd7311b3f6954afe691f1102b6c662d5cd5
Vlad GURDIGA <gurdiga@gmail.com>
2014-08-08 15:18:43 +0300

    Inquirer: inline util/attach-columns and util/extract-rows

--- app/inquirer.js
+++ app/inquirer.js
@@ -29,12 +29,27 @@ function inquireSection(section) {
   };
 }
 
+function extractRows(rowPreprocessor, subsectionName) {
+  rowPreprocessor = rowPreprocessor || function noop(row) { return row; };
+
+  return function(result) {
+    return result.rows.map(function(row) {
+      return rowPreprocessor(row.cell, row.id, subsectionName);
+    });
+  };
+}
+
+function attachColumns(columns) {
+  return function(rows) {
+    rows.columns = columns;
+    return rows;
+  };
+}
+
 var SupremeCourt = require('./supreme-court');
 var DistrictCourts = require('./district-courts');
 
 var forEach = require('./util/for-each');
 var queryAPI = require('./util/query-api');
-var attachColumns = require('./util/attach-columns');
-var extractRows = require('./util/extract-rows');
 
 module.exports = Inquirer;

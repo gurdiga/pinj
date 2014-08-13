8496a6f26ca905cfa44511669a84868202e3a931
Vlad GURDIGA <gurdiga@gmail.com>
2014-08-13 19:17:11 +0300

    make jshint-web-ui

--- makefiles/jshint/jshint.mk
+++ makefiles/jshint/jshint.mk
@@ -1,7 +1,10 @@
-jshint: makefiles/jshint/jshintrc.json
+jshint: makefiles/jshint/jshintrc.json jshint-web-ui
 
 makefiles/jshint/jshintrc.json: $(JSHINT_FILES)
 	@clear; jshint -c $@ $? && touch $@
 
 jshint-force:
 	@clear; jshint -c makefiles/jshint/jshintrc.json $(JSHINT_FILES)
+
+jshint-web-ui: $(JSHINT_WEB_FILES)
+	@clear; jshint -c app/web-ui/jshintrc.json $? && touch app/web-ui/jshintrc.json

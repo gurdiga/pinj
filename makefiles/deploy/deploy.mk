0dcaaf68de1f34a607f43d9f891183545d8a27e1
Vlad GURDIGA <gurdiga@gmail.com>
2014-09-12 17:51:40 +0300

    Update the deploy task

--- makefiles/deploy/deploy.mk
+++ makefiles/deploy/deploy.mk
@@ -1,5 +1,5 @@
 deploy: quick-deploy
-	ssh -p59922 pinj@pinj.pentru.md 'cd src/pinj; make setup'
+	ssh -p59922 pinj@dev.pentru.md 'cd src/pinj; make setup'
 
 quick-deploy:
-	ssh -p59922 pinj@pinj.pentru.md 'cd src/pinj; git checkout .; git pull origin $(BRANCH)'
+	ssh -p59922 pinj@dev.pentru.md 'cd src/pinj; git checkout .; git pull origin $(BRANCH)'

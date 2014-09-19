c94da600c26941769177324a6abf8acd1c9f07c3
Vlad GURDIGA <gurdiga@gmail.com>
2014-09-19 14:06:24 +0300

    Logout button: test: stub userService

--- test/logout-button.js
+++ test/logout-button.js
@@ -2,22 +2,25 @@
   'use strict';
 
   describe('Logout button', function() {
-    var LogoutButton, Deferred, App;
-    var logoutButton, domElement;
+    var LogoutButton, Deferred, UserService;
+    var logoutButton, domElement, userService;
 
     beforeEach(function() {
       LogoutButton = this.iframe.LogoutButton;
       Deferred = this.iframe.Deferred;
-      App = this.iframe.App;
+      UserService = this.iframe.UserService;
 
-      this.sinon.stub(App.userService, 'logout', Deferred.createResolvedPromise);
+      userService = sinon.createStubInstance(UserService);
+      MicroEvent.mixin(userService);
+      userService.logout.returns(Deferred.createResolvedPromise());
+
       domElement = document.createElement('button');
-      logoutButton = new LogoutButton(domElement, App.userService);
+      logoutButton = new LogoutButton(domElement, userService);
     });
 
     it('listens for the “click” events on the given element and calls UserService#logout', function() {
       domElement.click();
-      expect(App.userService.logout).to.have.been.called;
+      expect(userService.logout).to.have.been.called;
     });
   });
 

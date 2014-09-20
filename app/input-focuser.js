cf43a74e65a49a4f7a01c59b4614fc29fb9c6347
Vlad GURDIGA <gurdiga@gmail.com>
2014-09-20 16:40:51 +0300

    InputFocuser: refactoring

--- app/input-focuser.js
+++ app/input-focuser.js
@@ -2,17 +2,24 @@
   'use strict';
 
   function InputFocuser(tabLabels) {
+    this.listenForTabSwitchOn(tabLabels);
+  }
+
+  InputFocuser.DELAY = 300;
+
+  InputFocuser.prototype.listenForTabSwitchOn = function(tabLabels) {
     tabLabels.on('shown.bs.tab', function(event) {
       var context = tabLabels[0].ownerDocument.body;
       var tabContent = jQuery(event.target.getAttribute('href'), context);
+      this.focusTheFirstField(tabContent);
+    }.bind(this));
+  };
 
-      setTimeout(function() {
-        tabContent.find('input:first').focus();
-      }, InputFocuser.DELAY);
-    });
-  }
-
-  InputFocuser.DELAY = 500;
+  InputFocuser.prototype.focusTheFirstField = function(tabContent) {
+    setTimeout(function() {
+      tabContent.find('input:first').focus();
+    }, InputFocuser.DELAY);
+  };
 
   window.InputFocuser = InputFocuser;
 

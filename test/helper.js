24febfc884f9ed97380bb410525555ae44e34f24
Vlad GURDIGA <gurdiga@gmail.com>
2015-01-20 09:15:55 +0200

    SearchHistory: first test

--- test/helper.js
+++ test/helper.js
@@ -8,3 +8,6 @@ global.proxyquire = require('proxyquire');
 chai.use(require('chai-as-promised'));
 chai.use(require('sinon-chai'));
 require('mocha-sinon');
+
+var Q = require('q');
+require('sinon-as-promised')(Q.Promise);

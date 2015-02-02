2d3a5415df3d4aec2a647f5f73b0a80d3a512ba0
Vlad GURDIGA <gurdiga@gmail.com>
2015-02-02 06:52:08 +0200

    Sendig emails: less promises

--- app/send-emails.js
+++ app/send-emails.js
@@ -6,16 +6,11 @@ function sendEmails(emails) {
   return forEach(emails).inParallel(function(meta) {
     var email = meta.label;
     var subject = 'Monitorul PINJ: informaţii despre clienţi';
+    var bodies = meta.results;
 
-    return Promise.resolve(extractBodies(meta))
-    .then(sendEmail(email, subject));
+    return sendEmail(email, subject)(bodies);
   });
-}
-
-function extractBodies(meta) {
-  return meta.results;
 }
 
 var forEach = require('app/util/for-each');
 var sendEmail = require('app/util/send-email');
-var Promise = require('app/util/promise');

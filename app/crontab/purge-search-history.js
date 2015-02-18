c1d4f2e159d0ad6fcc01c8c6a19eadbf6c5bebb4
Vlad GURDIGA <gurdiga@gmail.com>
2015-02-18 19:06:30 +0200

    history purging

--- app/crontab/purge-search-history.js
+++ app/crontab/purge-search-history.js
@@ -1,7 +1,47 @@
 'use strict';
 
 function main() {
-  console.log('purging search history');
+  return getUserList()
+  .then(purgeSearchHistory)
+  .then(disconnectFirebase)
+  .catch(logErrorAndExit);
 }
+
+function purgeSearchHistory(users) {
+  console.log('. purging search history');
+
+  return forEach(users).inSeries(purgeSearchHistoryForUser);
+}
+
+function purgeSearchHistoryForUser(user) {
+  var path = '/search-history/' + user.aid;
+
+  return Data.get(path)
+  .then(function(results) {
+    var resultTimestamps = _(results).keys();
+    var oldResultTimestamps = _.initial(resultTimestamps, config.MAX_HISTORY_LENGTH);
+
+    return time(forEach(oldResultTimestamps).inSeries(function(timestamp) {
+      return Data.set(path + '/' + timestamp, null);
+    }), '.. ' + user.email);
+  });
+}
+
+function disconnectFirebase() {
+  // Prevent Firebase from hanging node.
+  process.exit();
+}
+
+function logErrorAndExit(error) {
+  console.error(error.stack);
+  process.exit(1);
+}
+
+var _ = require('underscore');
+var getUserList = require('app/get-user-lists');
+var Data = require('app/util/data');
+var forEach = require('app/util/for-each');
+var time = require('app/util/time');
+var config = require('app/config');
 
 main();

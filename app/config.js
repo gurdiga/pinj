7ad108a999753603ee5713ddeb135f8d2799348c
Vlad GURDIGA <gurdiga@gmail.com>
2015-05-30 01:11:11 +0300

    Subscription changes
    trial period: 3 months
    subscription period: 12 monts
    grace period: 1 month

--- app/config.js
+++ app/config.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-  'TRIAL_PERIOD'       : 31 * 24 * 3600 * 1000,
-  'SUBSCRIPTION_PERIOD': 31 * 24 * 3600 * 1000,
-  'GRACE_PERIOD'       : 7  * 24 * 3600 * 1000,
+  'TRIAL_PERIOD'       :  3 * 31 * 24 * 3600 * 1000,
+  'SUBSCRIPTION_PERIOD': 12 * 31 * 24 * 3600 * 1000,
+  'GRACE_PERIOD'       :  1 * 31 * 24 * 3600 * 1000,
   'TIME_BEFORE_THE_COVER_RUN': 3600 * 1000,
   'CLIENT_MIN_LENGTH': 5,
   'MAX_HISTORY_LENGTH': 3

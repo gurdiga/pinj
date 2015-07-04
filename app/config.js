f51c4822d3169e0672814f44c4e2eefb1acfd792
Vlad GURDIGA <gurdiga@gmail.com>
2015-07-05 02:18:38 +0300

    Grace period: one week

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@
 module.exports = {
   'TRIAL_PERIOD'       :  3 * 31 * 24 * 3600 * 1000,
   'SUBSCRIPTION_PERIOD': 12 * 31 * 24 * 3600 * 1000,
-  'GRACE_PERIOD'       :  1 * 31 * 24 * 3600 * 1000,
+  'GRACE_PERIOD'       :  1 *  7 * 24 * 3600 * 1000,
   'CLIENT_MIN_LENGTH': 5,
   'MAX_HISTORY_LENGTH': 3
 };
